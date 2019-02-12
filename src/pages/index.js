6c2f6abd38e8abea85546c43311a8d64a8722159
Tony Onodi <tonyonodi@gmail.com>
2019-02-12 17:13:03 +0000

    Moved static assets.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 import SEO from "../components/seo";
 import Project, {
   ProjectTitle,
